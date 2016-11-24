5c2e95b8bdf764d28413b12524c95100b355520d
Spencer Gibb <spencer@gibb.us>
2016-11-23 21:05:50 -0700

    initial gateway actuator endpoint

---
+++ src/main/java/org/springframework/cloud/gateway/actuate/GatewayEndpoint.java
@@ -0,0 +1,38 @@
+package org.springframework.cloud.gateway.actuate;
+
+import org.springframework.boot.actuate.endpoint.AbstractEndpoint;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.core.annotation.AnnotationAwareOrderComparator;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Spencer Gibb
+ */
+@ConfigurationProperties(prefix = "endpoints.gateway")
+public class GatewayEndpoint extends AbstractEndpoint<Map<String, Object>> {
+	private List<GatewayFilter> filters;
+
+	public GatewayEndpoint(List<GatewayFilter> filters) {
+		super("gateway");
+		this.filters = filters;
+		AnnotationAwareOrderComparator.sort(this.filters);
+	}
+
+	@Override
+	public Map<String, Object> invoke() {
+		HashMap<String, Object> map = new HashMap<>();
+
+		ArrayList<String> filterNames = new ArrayList<>();
+		for (GatewayFilter filter : this.filters) {
+			filterNames.add(filter.getClass().getName());
+		}
+		map.put("filters", filterNames);
+
+		return map;
+	}
+}
--- src/main/java/org/springframework/cloud/gateway/config/GatewayAutoConfiguration.java
+++ src/main/java/org/springframework/cloud/gateway/config/GatewayAutoConfiguration.java
@@ -1,7 +1,10 @@
 package org.springframework.cloud.gateway.config;
 
+import org.springframework.boot.actuate.endpoint.Endpoint;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.gateway.actuate.GatewayEndpoint;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.RouteToUrlFilter;
 import org.springframework.cloud.gateway.handler.GatewayFilteringWebHandler;
@@ -12,6 +15,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.client.reactive.WebClient;
+
+import java.util.List;
 
 /**
  * @author Spencer Gibb
@@ -54,6 +59,16 @@ public class GatewayAutoConfiguration {
 	@Bean
 	public GatewayHostHandlerMapping gatewayHostHandlerMapping(GatewayProperties properties, GatewayFilteringWebHandler webHandler) {
 		return new GatewayHostHandlerMapping(properties, webHandler);
+	}
+
+	@Configuration
+	@ConditionalOnClass(Endpoint.class)
+	protected static class GatewayActuatorConfiguration {
+
+		@Bean
+		public GatewayEndpoint gatewayEndpoint(List<GatewayFilter> filters) {
+			return new GatewayEndpoint(filters);
+		}
 	}
 
 }
