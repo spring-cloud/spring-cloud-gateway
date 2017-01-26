111cca2cb5e13b7b8e83b4f18312e535007a1aff
Spencer Gibb <spencer@gibb.us>
2017-01-26 15:16:59 -0700

    polish

--- src/main/java/org/springframework/cloud/gateway/config/GatewayEnvironmentPostProcessor.java
+++ src/main/java/org/springframework/cloud/gateway/config/GatewayEnvironmentPostProcessor.java
@@ -1,29 +1,20 @@
 package org.springframework.cloud.gateway.config;
 
+import java.util.Collections;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.core.Ordered;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MapPropertySource;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * @author Spencer Gibb
  */
 public class GatewayEnvironmentPostProcessor implements EnvironmentPostProcessor, Ordered {
 
-	private static final Map<String, Object> PROPERTIES;
-
 	public static final int DEFAULT_ORDER = Ordered.HIGHEST_PRECEDENCE + 10;
-
-	static {
-		Map<String, Object> properties = new HashMap<>();
-		properties.put("spring.resources.add-mappings", "false");
-		PROPERTIES = Collections.unmodifiableMap(properties);
-	}
 
 	@Override
 	public int getOrder() {
@@ -34,7 +25,8 @@ public class GatewayEnvironmentPostProcessor implements EnvironmentPostProcessor
 	public void postProcessEnvironment(ConfigurableEnvironment configurableEnvironment, SpringApplication springApplication) {
 		//TODO: make this configurable?
 		//TODO: should I have to do this?
-		configurableEnvironment.getPropertySources().addLast(new MapPropertySource("gateway", PROPERTIES));
+		final Map<String, Object> properties = Collections.singletonMap("spring.resources.add-mappings", "false");
+		configurableEnvironment.getPropertySources().addLast(new MapPropertySource("gateway", properties));
 	}
 }
 
