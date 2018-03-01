16c8a07d7c8d39f5c677afbc189abe578d41d75a
Biju Kunjummen <biju.kunjummen@gmail.com>
2018-02-28 19:15:11 -0800

    Derive kotlin version from parent (#212)

--- spring-cloud-gateway-sample/src/main/kotlin/org/springframework/cloud/gateway/sample/AdditionalRoutes.kt
+++ spring-cloud-gateway-sample/src/main/kotlin/org/springframework/cloud/gateway/sample/AdditionalRoutes.kt
@@ -1,6 +1,5 @@
 package org.springframework.cloud.gateway.sample
 
-import org.springframework.cloud.gateway.route.RouteLocator
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.cloud.gateway.route.builder.filters
 import org.springframework.cloud.gateway.route.builder.routes
@@ -8,10 +7,10 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class AdditionalRoutes {
+open class AdditionalRoutes {
 
 	@Bean
-	fun additionalRouteLocator(builder: RouteLocatorBuilder): RouteLocator = builder.routes {
+	open fun additionalRouteLocator(builder: RouteLocatorBuilder) = builder.routes {
 		route(id = "test-kotlin") {
 			host("kotlin.abc.org") and path("/image/png")
 			filters {
