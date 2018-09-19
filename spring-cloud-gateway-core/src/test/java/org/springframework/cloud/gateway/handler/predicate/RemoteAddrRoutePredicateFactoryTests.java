f884e03963066f73b4606114f0c14ba64b19a126
Erko Risthein <erko@risthein.ee>
2018-09-19 17:25:49 +0300

    Fix #536 and add an integration test that reproduces the bug

--- spring-cloud-gateway-core/src/test/java/org/springframework/cloud/gateway/handler/predicate/RemoteAddrRoutePredicateFactoryTests.java
+++ spring-cloud-gateway-core/src/test/java/org/springframework/cloud/gateway/handler/predicate/RemoteAddrRoutePredicateFactoryTests.java
@@ -5,10 +5,15 @@ import static org.springframework.cloud.gateway.test.TestUtils.assertStatus;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.gateway.support.ipresolver.XForwardedRemoteAddressResolver;
 import org.springframework.cloud.gateway.test.BaseWebClientTests;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.annotation.DirtiesContext;
@@ -44,10 +49,31 @@ public class RemoteAddrRoutePredicateFactoryTests extends BaseWebClientTests {
 				.expectComplete().verify(DURATION);
 	}
 
+	@Test
+	public void remoteAddrWorksWithXForwardedRemoteAddress() {
+		Mono<ClientResponse> result = webClient.get().uri("/xforwardfor")
+				.header("X-Forwarded-For", "12.34.56.78").exchange();
+
+		StepVerifier.create(result)
+				.consumeNextWith(response -> assertStatus(response, HttpStatus.OK))
+				.expectComplete().verify(DURATION);
+	}
+
 	@EnableAutoConfiguration
 	@SpringBootConfiguration
 	@Import(DefaultTestConfig.class)
 	public static class TestConfig {
+		@Value("${test.uri}")
+		String uri;
+
+		@Bean
+		public RouteLocator testRouteLocator(RouteLocatorBuilder builder) {
+			return builder.routes().route("x_forwarded_for_test", r -> r
+					.path("/xforwardfor").and()
+					.remoteAddr(XForwardedRemoteAddressResolver.maxTrustedIndex(1),
+							"12.34.56.78")
+					.filters(f -> f.setStatus(200)).uri(uri)).build();
+		}
 	}
 
 }
