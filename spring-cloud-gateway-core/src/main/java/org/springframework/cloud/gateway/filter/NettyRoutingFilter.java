9eea694f9f8fba82a0b8974149544b6137b8fa95
Ryan Baxter <rbaxter@pivotal.io>
2019-06-24 11:27:02 -0400

    Get ASCII string in netty routing filter.  Fixes #1132.

--- spring-cloud-gateway-core/src/main/java/org/springframework/cloud/gateway/filter/NettyRoutingFilter.java
+++ spring-cloud-gateway-core/src/main/java/org/springframework/cloud/gateway/filter/NettyRoutingFilter.java
@@ -107,7 +107,7 @@ public class NettyRoutingFilter implements GlobalFilter, Ordered {
 		ServerHttpRequest request = exchange.getRequest();
 
 		final HttpMethod method = HttpMethod.valueOf(request.getMethodValue());
-		final String url = requestUrl.toString();
+		final String url = requestUrl.toASCIIString();
 
 		HttpHeaders filtered = filterRequest(getHeadersFilters(), exchange);
 
