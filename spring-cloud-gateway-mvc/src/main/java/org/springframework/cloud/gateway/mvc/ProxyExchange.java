8abc6b71cee26edbd7d9b6ca5d555fcd1ad8919e
如梦技术 <596392912@qq.com>
2019-07-25 13:02:37 +0800

    update gateway mvc post, put, and patch request body not required.

--- spring-cloud-gateway-mvc/src/main/java/org/springframework/cloud/gateway/mvc/ProxyExchange.java
+++ spring-cloud-gateway-mvc/src/main/java/org/springframework/cloud/gateway/mvc/ProxyExchange.java
@@ -462,7 +462,7 @@ public class ProxyExchange<T> {
 
 	protected static class BodyGrabber {
 
-		public Object body(@RequestBody Object body) {
+		public Object body(@RequestBody(required = false) Object body) {
 			return body;
 		}
 
