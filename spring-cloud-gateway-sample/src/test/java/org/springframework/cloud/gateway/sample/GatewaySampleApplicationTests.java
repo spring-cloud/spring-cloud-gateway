d57c97f6d6f05a46c45085a0f5e4f766fd05eb68
buildmaster <buildmaster@springframework.org>
2020-01-22 05:21:50 +0000

    Bumping versions

--- spring-cloud-gateway-sample/src/test/java/org/springframework/cloud/gateway/sample/GatewaySampleApplicationTests.java
+++ spring-cloud-gateway-sample/src/test/java/org/springframework/cloud/gateway/sample/GatewaySampleApplicationTests.java
@@ -49,8 +49,8 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
  * @author Spencer Gibb
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {
-		GatewaySampleApplicationTests.TestConfig.class }, webEnvironment = RANDOM_PORT, properties = "management.server.port=${test.port}")
+@SpringBootTest(classes = { GatewaySampleApplicationTests.TestConfig.class },
+		webEnvironment = RANDOM_PORT, properties = "management.server.port=${test.port}")
 public class GatewaySampleApplicationTests {
 
 	protected static int managementPort;
@@ -195,8 +195,10 @@ public class GatewaySampleApplicationTests {
 		int port;
 
 		@Bean
-		public ServiceInstanceListSupplier fixedServiceInstanceListSupplier(Environment env) {
-			return ServiceInstanceListSupplier.fixed(env).instance("localhost", port, "httpbin").build();
+		public ServiceInstanceListSupplier fixedServiceInstanceListSupplier(
+				Environment env) {
+			return ServiceInstanceListSupplier.fixed(env)
+					.instance("localhost", port, "httpbin").build();
 		}
 
 	}
