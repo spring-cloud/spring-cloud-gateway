fa70b31f1ed80bf8a30e598b8dc62b9cd977547e
Alvin <329772643@qq.com>
2020-05-20 03:24:53 +0800

    Cache the success routes until next successful refresh. (#1602)
    * refreshWorksWhenFirstRefreshSuccessAndOtherError
    
    * update format
    
    * use isSuccess instred of Enum
    
    Co-authored-by: 田雪峰 <tianxuefeng@shuidihuzhu.com>
    
    Fixes gh-600

--- spring-cloud-gateway-core/src/test/java/org/springframework/cloud/gateway/route/CachingRouteLocatorTests.java
+++ spring-cloud-gateway-core/src/test/java/org/springframework/cloud/gateway/route/CachingRouteLocatorTests.java
@@ -16,10 +16,16 @@
 
 package org.springframework.cloud.gateway.route;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
 import reactor.core.publisher.Flux;
+
+import org.springframework.cloud.gateway.event.RefreshRoutesEvent;
+import org.springframework.cloud.gateway.event.RefreshRoutesResultEvent;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -58,6 +64,73 @@ public class CachingRouteLocatorTests {
 
 		routes = locator.refresh().collectList().block();
 		assertThat(routes).containsExactly(route1, route2);
+	}
+
+	@Test
+	public void refreshWorksWhenFirstRefreshSuccessAndOtherError()
+			throws InterruptedException {
+		Route route1 = route(1);
+		Route route2 = route(2);
+
+		CachingRouteLocator locator = new CachingRouteLocator(new RouteLocator() {
+			int i = 0;
+
+			@Override
+			public Flux<Route> getRoutes() {
+				if (i == 0) {
+					i++;
+					return Flux.just(route1);
+				}
+				else if (i == 1) {
+					i++;
+					return Flux.just(route2).map(route -> {
+						throw new RuntimeException("in chain.");
+					});
+				}
+				else if (i == 2) {
+					i++;
+					throw new RuntimeException("call getRoutes error.");
+				}
+				return Flux.just(route2);
+			}
+		});
+
+		List<Route> routes = locator.getRoutes().collectList().block();
+		assertThat(routes).containsExactly(route1);
+
+		List<RefreshRoutesResultEvent> resultEvents = new ArrayList<>();
+
+		waitUntilRefreshFinished(locator, resultEvents);
+		assertThat(resultEvents).hasSize(1);
+		assertThat(resultEvents.get(0).getThrowable().getCause().getMessage())
+				.isEqualTo("in chain.");
+		assertThat(resultEvents.get(0).isSuccess()).isEqualTo(false);
+		assertThat(locator.getRoutes().collectList().block()).containsExactly(route1);
+
+		waitUntilRefreshFinished(locator, resultEvents);
+		assertThat(resultEvents).hasSize(2);
+		assertThat(resultEvents.get(1).getThrowable().getMessage())
+				.isEqualTo("call getRoutes error.");
+		assertThat(resultEvents.get(1).isSuccess()).isEqualTo(false);
+		assertThat(locator.getRoutes().collectList().block()).containsExactly(route1);
+
+		waitUntilRefreshFinished(locator, resultEvents);
+		assertThat(resultEvents).hasSize(3);
+		assertThat(resultEvents.get(2).isSuccess()).isEqualTo(true);
+		assertThat(locator.getRoutes().collectList().block()).containsExactly(route2);
+
+	}
+
+	private void waitUntilRefreshFinished(CachingRouteLocator locator,
+			List<RefreshRoutesResultEvent> resultEvents) throws InterruptedException {
+		CountDownLatch cdl = new CountDownLatch(1);
+		locator.setApplicationEventPublisher(o -> {
+			resultEvents.add((RefreshRoutesResultEvent) o);
+			cdl.countDown();
+		});
+		locator.onApplicationEvent(new RefreshRoutesEvent(this));
+
+		assertThat(cdl.await(5, TimeUnit.SECONDS)).isTrue();
 	}
 
 	Route route(int id) {
