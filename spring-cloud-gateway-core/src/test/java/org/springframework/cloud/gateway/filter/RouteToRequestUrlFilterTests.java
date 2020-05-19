0b71884ca4c9866c1851298c032b6853a0c8bb76
spencergibb <spencer@gibb.us>
2020-05-19 12:58:21 -0400

    Ignores test for boot 2.2.x

--- spring-cloud-gateway-core/src/test/java/org/springframework/cloud/gateway/filter/RouteToRequestUrlFilterTests.java
+++ spring-cloud-gateway-core/src/test/java/org/springframework/cloud/gateway/filter/RouteToRequestUrlFilterTests.java
@@ -22,6 +22,7 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import reactor.core.publisher.Mono;
 
+import org.springframework.boot.SpringBootVersion;
 import org.springframework.cloud.gateway.route.Route;
 import org.springframework.http.HttpMethod;
 import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
@@ -30,6 +31,7 @@ import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assume.assumeTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR;
@@ -116,6 +118,9 @@ public class RouteToRequestUrlFilterTests {
 
 	@Test
 	public void partialEncodedParameters() {
+		assumeTrue("partialEncodedParameters ignored for boot 2.2",
+				SpringBootVersion.getVersion().startsWith("2.3."));
+
 		URI url = UriComponentsBuilder
 				.fromUriString("http://localhost/get?key[]=test= key&start=1533108081")
 				.build().toUri();
