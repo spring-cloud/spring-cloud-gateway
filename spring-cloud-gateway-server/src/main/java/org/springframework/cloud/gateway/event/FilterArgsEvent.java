7423791de382a788246bb6f277d5616425e8e6d8
spencergibb <spencer@gibb.us>
2020-06-29 17:38:00 -0400

    Migrates from spring-cloud-gateway-core to spring-cloud-gateway-server.

---
+++ spring-cloud-gateway-server/src/main/java/org/springframework/cloud/gateway/event/FilterArgsEvent.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2013-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.gateway.event;
+
+import java.util.Map;
+
+import org.springframework.context.ApplicationEvent;
+
+public class FilterArgsEvent extends ApplicationEvent {
+
+	private final Map<String, Object> args;
+
+	private String routeId;
+
+	public FilterArgsEvent(Object source, String routeId, Map<String, Object> args) {
+		super(source);
+		this.routeId = routeId;
+		this.args = args;
+	}
+
+	public String getRouteId() {
+		return routeId;
+	}
+
+	public Map<String, Object> getArgs() {
+		return args;
+	}
+
+}
---
+++ spring-cloud-gateway-server/src/main/java/org/springframework/cloud/gateway/filter/factory/AbstractGatewayFilterFactory.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2013-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.gateway.filter.factory;
+
+import org.springframework.cloud.gateway.support.AbstractConfigurable;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+
+/**
+ * This class is BETA and may be subject to change in a future release.
+ *
+ * @param <C> {@link AbstractConfigurable} subtype
+ */
+public abstract class AbstractGatewayFilterFactory<C> extends AbstractConfigurable<C>
+		implements GatewayFilterFactory<C>, ApplicationEventPublisherAware {
+
+	private ApplicationEventPublisher publisher;
+
+	@SuppressWarnings("unchecked")
+	public AbstractGatewayFilterFactory() {
+		super((Class<C>) Object.class);
+	}
+
+	public AbstractGatewayFilterFactory(Class<C> configClass) {
+		super(configClass);
+	}
+
+	protected ApplicationEventPublisher getPublisher() {
+		return this.publisher;
+	}
+
+	@Override
+	public void setApplicationEventPublisher(ApplicationEventPublisher publisher) {
+		this.publisher = publisher;
+	}
+
+	public static class NameConfig {
+
+		private String name;
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+	}
+
+}
