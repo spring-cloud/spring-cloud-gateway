91e475e65fa2eba190ba06c89c555ed39e414ec8
spencergibb <spencer@gibb.us>
2021-03-12 16:05:37 -0500

    Adds routeId to toString() and formatting

--- spring-cloud-gateway-server/src/main/java/org/springframework/cloud/gateway/filter/factory/RetryGatewayFilterFactory.java
+++ spring-cloud-gateway-server/src/main/java/org/springframework/cloud/gateway/filter/factory/RetryGatewayFilterFactory.java
@@ -168,10 +168,13 @@ public class RetryGatewayFilterFactory
 
 			@Override
 			public String toString() {
-				return filterToStringCreator(RetryGatewayFilterFactory.this).append("retries", retryConfig.getRetries())
-						.append("series", retryConfig.getSeries()).append("statuses", retryConfig.getStatuses())
-						.append("methods", retryConfig.getMethods()).append("exceptions", retryConfig.getExceptions())
-						.toString();
+				return filterToStringCreator(RetryGatewayFilterFactory.this)
+						.append("routeId", retryConfig.getRouteId())
+						.append("retries", retryConfig.getRetries())
+						.append("series", retryConfig.getSeries())
+						.append("statuses", retryConfig.getStatuses())
+						.append("methods", retryConfig.getMethods())
+						.append("exceptions", retryConfig.getExceptions()).toString();
 			}
 		};
 	}
