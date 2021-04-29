529c750f989f19b1b62cc0a4c6b32e483aced709
zhenqiangyi <835576511@qq.com>
2021-04-29 16:54:52 +0800

    Optimize the unit test of AbstractCustomizeRouteResolveHandlerMapping

--- spring-cloud-gateway-server/src/test/java/org/springframework/cloud/gateway/handler/CustomizeRouteResolveHandlerMappingTest.java
+++ spring-cloud-gateway-server/src/test/java/org/springframework/cloud/gateway/handler/CustomizeRouteResolveHandlerMappingTest.java
@@ -16,33 +16,54 @@
 
 package org.springframework.cloud.gateway.handler;
 
-import java.net.URI;
-
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import reactor.core.publisher.Flux;
 
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.gateway.route.RouteDefinition;
-import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
-import org.springframework.cloud.gateway.test.BaseWebClientTests;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.gateway.test.BaseWebClientTests;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.SocketUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ServerWebExchange;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = RANDOM_PORT, properties = {})
+@SpringBootTest(webEnvironment = RANDOM_PORT, properties = "management.server.port=${test.port}")
 @DirtiesContext
 public class CustomizeRouteResolveHandlerMappingTest extends BaseWebClientTests {
 
+	private static int managementPort;
+
+	@BeforeClass
+	public static void beforeClass() {
+		managementPort = SocketUtils.findAvailableTcpPort();
+		System.setProperty("test.port", String.valueOf(managementPort));
+	}
+
+	@AfterClass
+	public static void afterClass() {
+		System.clearProperty("test.port");
+	}
+
 	@Test
 	public void requestsToCustomizeRoute() {
+		testClient.get().uri("/openapi?apiName=openapi.trade-order.create&apiVersion=1.0.0&orderId=123456")
+				.exchange().expectBody(String.class).isEqualTo("123456");
+
+		testClient.get().uri("/openapi?apiName=openapi.trade-order.delete&apiVersion=1.0.0&orderId=6666666")
+				.exchange().expectBody(String.class).isEqualTo("6666666");
 	}
 
 	@EnableAutoConfiguration
@@ -50,22 +71,42 @@ public class CustomizeRouteResolveHandlerMappingTest extends BaseWebClientTests 
 	@Import(DefaultTestConfig.class)
 	public static class TestConfig {
 
+		@GetMapping("/trade-order/create")
+		String create(@RequestParam(name = "orderId", defaultValue = "123456") String orderId) {
+			return orderId;
+		}
+
+		@GetMapping("/trade-order/delete")
+		String delete(@RequestParam(name = "orderId", defaultValue = "123456") String orderId) {
+			return orderId;
+		}
+
 		@Bean
 		AbstractCustomizeRouteResolveHandlerMapping myCustomizeRouteResolveHandlerMapping() {
 			return new AbstractCustomizeRouteResolveHandlerMapping() {
 				@Override
 				protected String resolveRouteId(ServerWebExchange serverWebExchange) {
-					return "one_customize_route";
+					MultiValueMap<String, String> queryParams = serverWebExchange.getRequest().getQueryParams();
+					String apiName = queryParams.getFirst("apiName");
+					String apiVersion = queryParams.getFirst("apiVersion");
+
+					String routeId = apiName + "#" + apiVersion;
+
+					return routeId;
 				}
 			};
 		}
 
 		@Bean
-		RouteDefinitionLocator myRouteDefinitionLocator() {
-			RouteDefinition definition = new RouteDefinition();
-			definition.setId("one_customize_route");
-			definition.setUri(URI.create("https://oneapi"));
-			return () -> Flux.just(definition);
+		RouteLocator testRouteLocator(RouteLocatorBuilder builder) {
+			return builder.routes()
+					.route("openapi.trade-order.create#1.0.0",
+							r -> r.query("apiName","openapi.trade-order.create").and()
+									.query("apiVersion","1.0.0").uri("/trade-order/create"))
+					.route("openapi.trade-order.delete#1.0.0",
+							r -> r.query("apiName","openapi.trade-order.delete").and()
+									.query("apiVersion","1.0.0").uri("/trade-order/delete"))
+					.build();
 		}
 	}
 }
