907119dfa38ffc1734a6e898935c63b389d995f7
Olga Maciaszek-Sharma <olga.maciaszek@gmail.com>
2021-07-02 19:00:46 +0200

    Fix 2214 hoxton (#2285)

--- spring-cloud-gateway-server/src/main/java/org/springframework/cloud/gateway/filter/headers/ForwardedHeadersFilter.java
+++ spring-cloud-gateway-server/src/main/java/org/springframework/cloud/gateway/filter/headers/ForwardedHeadersFilter.java
@@ -16,6 +16,8 @@
 
 package org.springframework.cloud.gateway.filter.headers;
 
+import java.net.Inet6Address;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.URI;
 import java.util.ArrayList;
@@ -120,8 +122,17 @@ public class ForwardedHeadersFilter implements HttpHeadersFilter, Ordered {
 		if (remoteAddress != null) {
 			// If remoteAddress is unresolved, calling getHostAddress() would cause a
 			// NullPointerException.
-			String forValue = remoteAddress.isUnresolved() ? remoteAddress.getHostName()
-					: remoteAddress.getAddress().getHostAddress();
+			String forValue;
+			if (remoteAddress.isUnresolved()) {
+				forValue = remoteAddress.getHostName();
+			}
+			else {
+				InetAddress address = remoteAddress.getAddress();
+				forValue = remoteAddress.getAddress().getHostAddress();
+				if (address instanceof Inet6Address) {
+					forValue = "[" + forValue + "]";
+				}
+			}
 			int port = remoteAddress.getPort();
 			if (port >= 0) {
 				forValue = forValue + ":" + port;
