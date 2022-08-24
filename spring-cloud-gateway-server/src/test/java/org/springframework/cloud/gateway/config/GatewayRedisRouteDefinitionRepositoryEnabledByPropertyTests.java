c98a11f5fbc21362b490fe416353e859aaa203d2
spencergibb <sgibb@pivotal.io>
2022-08-24 13:14:22 -0400

    Moves to new redis prefix

--- spring-cloud-gateway-server/src/test/java/org/springframework/cloud/gateway/config/GatewayRedisRouteDefinitionRepositoryEnabledByPropertyTests.java
+++ spring-cloud-gateway-server/src/test/java/org/springframework/cloud/gateway/config/GatewayRedisRouteDefinitionRepositoryEnabledByPropertyTests.java
@@ -52,8 +52,8 @@ public class GatewayRedisRouteDefinitionRepositoryEnabledByPropertyTests {
 
 	@DynamicPropertySource
 	static void containerProperties(DynamicPropertyRegistry registry) {
-		registry.add("spring.redis.host", redis::getContainerIpAddress);
-		registry.add("spring.redis.port", redis::getFirstMappedPort);
+		registry.add("spring.data.redis.host", redis::getHost);
+		registry.add("spring.data.redis.port", redis::getFirstMappedPort);
 	}
 
 	@Test
--- spring-cloud-gateway-server/src/test/java/org/springframework/cloud/gateway/filter/ratelimit/RedisRateLimiterTests.java
+++ spring-cloud-gateway-server/src/test/java/org/springframework/cloud/gateway/filter/ratelimit/RedisRateLimiterTests.java
@@ -66,8 +66,8 @@ public class RedisRateLimiterTests extends BaseWebClientTests {
 
 	@DynamicPropertySource
 	static void containerProperties(DynamicPropertyRegistry registry) {
-		registry.add("spring.redis.host", redis::getContainerIpAddress);
-		registry.add("spring.redis.port", redis::getFirstMappedPort);
+		registry.add("spring.data.redis.host", redis::getHost);
+		registry.add("spring.data.redis.port", redis::getFirstMappedPort);
 	}
 
 	@BeforeEach
