4dba8bf1e4d1d904be63d7f37a0334aeb6480b0c
spencergibb <sgibb@pivotal.io>
2023-01-24 15:21:57 -0500

    Updates to instanceof with variable

--- spring-cloud-gateway-server/src/main/java/org/springframework/cloud/gateway/filter/factory/CacheRequestBodyGatewayFilterFactory.java
+++ spring-cloud-gateway-server/src/main/java/org/springframework/cloud/gateway/filter/factory/CacheRequestBodyGatewayFilterFactory.java
@@ -89,8 +89,8 @@ public class CacheRequestBodyGatewayFilterFactory
 							//
 							Object backupCachedBody = exchange.getAttributes()
 									.get(CACHED_ORIGINAL_REQUEST_BODY_BACKUP_ATTR);
-							if (backupCachedBody instanceof DataBuffer) {
-								DataBufferUtils.release((DataBuffer) backupCachedBody);
+							if (backupCachedBody instanceof DataBuffer dataBuffer) {
+								DataBufferUtils.release(dataBuffer);
 							}
 						});
 					}));
