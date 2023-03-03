75e5049c833f6d95062e7de9fe5826068fe9303b
spencergibb <sgibb@pivotal.io>
2023-03-03 16:55:35 -0500

    formatting

--- spring-cloud-gateway-server/src/main/java/org/springframework/cloud/gateway/filter/WebsocketRoutingFilter.java
+++ spring-cloud-gateway-server/src/main/java/org/springframework/cloud/gateway/filter/WebsocketRoutingFilter.java
@@ -209,36 +209,36 @@ public class WebsocketRoutingFilter implements GlobalFilter, Ordered {
 						return closeStatus;
 					}
 					switch (code) {
-					case 1000:
-					case 1001:
-					case 1002:
-					case 1003:
-					case 1007:
-					case 1008:
-					case 1009:
-					case 1010:
-					case 1011:
-						return closeStatus;
-					case 1004:
-						// Should not be used in a close frame
-						// RESERVED;
-					case 1005:
-						// Should not be used in a close frame
-						// return CloseStatus.NO_STATUS_CODE;
-					case 1006:
-						// Should not be used in a close frame
-						// return CloseStatus.NO_CLOSE_FRAME;
-					case 1012:
-						// Not in RFC6455
-						// return CloseStatus.SERVICE_RESTARTED;
-					case 1013:
-						// Not in RFC6455
-						// return CloseStatus.SERVICE_OVERLOAD;
-					case 1015:
-						// Should not be used in a close frame
-						// return CloseStatus.TLS_HANDSHAKE_FAILURE;
-					default:
-						return CloseStatus.PROTOCOL_ERROR;
+						case 1000:
+						case 1001:
+						case 1002:
+						case 1003:
+						case 1007:
+						case 1008:
+						case 1009:
+						case 1010:
+						case 1011:
+							return closeStatus;
+						case 1004:
+							// Should not be used in a close frame
+							// RESERVED;
+						case 1005:
+							// Should not be used in a close frame
+							// return CloseStatus.NO_STATUS_CODE;
+						case 1006:
+							// Should not be used in a close frame
+							// return CloseStatus.NO_CLOSE_FRAME;
+						case 1012:
+							// Not in RFC6455
+							// return CloseStatus.SERVICE_RESTARTED;
+						case 1013:
+							// Not in RFC6455
+							// return CloseStatus.SERVICE_OVERLOAD;
+						case 1015:
+							// Should not be used in a close frame
+							// return CloseStatus.TLS_HANDSHAKE_FAILURE;
+						default:
+							return CloseStatus.PROTOCOL_ERROR;
 					}
 				}
 
