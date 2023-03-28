4b7470728008053d96722edbf3cd3f2599e306b1
Ignacio Lozano <ilozano@vmware.com>
2023-03-28 17:05:17 +0200

    Add a new endpoint POST /batch/routes
    A list of RouteDefinition can be sent to /batch/routes using POST method to persist multiple routes by a single request.
    
    Validation is done before persisting any RouteDefinition

--- spring-cloud-gateway-server/src/main/java/org/springframework/cloud/gateway/actuate/AbstractGatewayControllerEndpoint.java
+++ spring-cloud-gateway-server/src/main/java/org/springframework/cloud/gateway/actuate/AbstractGatewayControllerEndpoint.java
@@ -145,6 +145,32 @@ public class AbstractGatewayControllerEndpoint implements ApplicationEventPublis
 				.switchIfEmpty(Mono.defer(() -> Mono.just(ResponseEntity.badRequest().build())));
 	}
 
+	@PostMapping("/batch/routes")
+	@SuppressWarnings("unchecked")
+	public Mono<ResponseEntity<Object>> save(@RequestBody List<RouteDefinition> routes) {
+		routes.stream()
+			  .forEach(routeDef -> {
+				  validateRouteDefinition(routeDef);
+				  validateRouteId(routeDef);
+			  });
+
+		return Flux.fromIterable(routes)
+				   .flatMap(routeDefinition ->
+						   this.routeDefinitionWriter.save(Mono.just(routeDefinition).map(r -> {
+							   log.debug("Saving route: " + routeDefinition);
+							   return r;
+						   })))
+				   // TODO Check
+				   .then(Mono.defer(() -> Mono.just(ResponseEntity.created(URI.create("/routes/" + routes.get(0).getId())).build())))
+				   .switchIfEmpty(Mono.defer(() -> Mono.just(ResponseEntity.badRequest().build())));
+	}
+
+	private void validateRouteId(RouteDefinition routeDefinition) {
+		if(routeDefinition.getId() == null) {
+			handleError("Saving multiple routes require specifying the ID for every route");
+		}
+	}
+
 	private void validateRouteDefinition(RouteDefinition routeDefinition) {
 		Set<String> unavailableFilterDefinitions = routeDefinition.getFilters().stream().filter(rd -> !isAvailable(rd))
 				.map(FilterDefinition::getName).collect(Collectors.toSet());
