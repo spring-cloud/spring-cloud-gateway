08be1a5b42417c88f66e32cdf0e5986099313b99
manzhizhen <dubbocommitter@gmail.com>
2023-07-23 17:15:01 +0800

    Optimize unnecessary reactive code to improve operational efficiency

--- spring-cloud-gateway-server/src/main/java/org/springframework/cloud/gateway/filter/factory/rewrite/CachedBodyOutputMessage.java
+++ spring-cloud-gateway-server/src/main/java/org/springframework/cloud/gateway/filter/factory/rewrite/CachedBodyOutputMessage.java
@@ -91,7 +91,7 @@ public class CachedBodyOutputMessage implements ReactiveHttpOutputMessage {
 
 	@Override
 	public Mono<Void> writeAndFlushWith(Publisher<? extends Publisher<? extends DataBuffer>> body) {
-		return writeWith(Flux.from(body).flatMap(p -> p));
+		return writeWith(Flux.from(body));
 	}
 
 	@Override
