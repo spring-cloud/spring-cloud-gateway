bb1f53ef30a3dd0fcc2abce3870a20ca00799ef3
sunhao1256 <sunhao1256@gmail.com>
2024-02-01 10:35:57 +0800

    feat: add condition replaceQueryParams for ProxyExchangeHandlerFunction constructor

--- spring-cloud-gateway-server-mvc/src/main/java/org/springframework/cloud/gateway/server/mvc/handler/ProxyExchangeHandlerFunction.java
+++ spring-cloud-gateway-server-mvc/src/main/java/org/springframework/cloud/gateway/server/mvc/handler/ProxyExchangeHandlerFunction.java
@@ -34,6 +34,8 @@ import org.springframework.web.servlet.function.ServerRequest;
 import org.springframework.web.servlet.function.ServerResponse;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.xml.transform.URIResolver;
+
 public class ProxyExchangeHandlerFunction implements HandlerFunction<ServerResponse> {
 
 	private static final Log log = LogFactory.getLog(ProxyExchangeHandlerFunction.class);
@@ -46,6 +48,8 @@ public class ProxyExchangeHandlerFunction implements HandlerFunction<ServerRespo
 
 	private final URIResolver uriResolver;
 
+	private final Boolean replaceQueryParams;
+
 	public ProxyExchangeHandlerFunction(ProxyExchange proxyExchange,
 			ObjectProvider<RequestHttpHeadersFilter> requestHttpHeadersFilters,
 			ObjectProvider<ResponseHttpHeadersFilter> responseHttpHeadersFilters) {
@@ -57,10 +61,18 @@ public class ProxyExchangeHandlerFunction implements HandlerFunction<ServerRespo
 	public ProxyExchangeHandlerFunction(ProxyExchange proxyExchange,
 			ObjectProvider<RequestHttpHeadersFilter> requestHttpHeadersFilters,
 			ObjectProvider<ResponseHttpHeadersFilter> responseHttpHeadersFilters, URIResolver uriResolver) {
+		this(proxyExchange,requestHttpHeadersFilters,responseHttpHeadersFilters,uriResolver,true);
+	}
+
+	public ProxyExchangeHandlerFunction(ProxyExchange proxyExchange,
+										ObjectProvider<RequestHttpHeadersFilter> requestHttpHeadersFilters,
+										ObjectProvider<ResponseHttpHeadersFilter> responseHttpHeadersFilters, URIResolver uriResolver,
+										Boolean replaceQueryParams) {
 		this.proxyExchange = proxyExchange;
 		this.requestHttpHeadersFilters = requestHttpHeadersFilters;
 		this.responseHttpHeadersFilters = responseHttpHeadersFilters;
 		this.uriResolver = uriResolver;
+		this.replaceQueryParams = replaceQueryParams;
 	}
 
 	@Override
@@ -68,13 +80,18 @@ public class ProxyExchangeHandlerFunction implements HandlerFunction<ServerRespo
 		URI uri = uriResolver.apply(serverRequest);
 		boolean encoded = containsEncodedQuery(serverRequest.uri());
 		// @formatter:off
-		URI url = UriComponentsBuilder.fromUri(serverRequest.uri())
+		 UriComponentsBuilder builder= UriComponentsBuilder.fromUri(serverRequest.uri())
 				.scheme(uri.getScheme())
 				.host(uri.getHost())
-				.port(uri.getPort())
-				.replaceQueryParams(serverRequest.params())
-				.build(encoded)
-				.toUri();
+				.port(uri.getPort());
+
+		 if (replaceQueryParams){
+			 builder.replaceQueryParams(serverRequest.params());
+		 }
+
+		 URI url = builder
+			 .build(encoded)
+			 .toUri();
 		// @formatter:on
 
 		// TODO: Streams.collect()?
