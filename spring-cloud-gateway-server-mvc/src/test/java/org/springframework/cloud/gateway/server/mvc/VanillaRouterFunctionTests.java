35c638475e91470f07521df04f1cac740f49b609
sgibb <sgibb@pivotal.io>
2024-03-13 19:31:51 -0400

    Avoids NPE in MvcUtils
    This was caused if vanilla RouterFunctions.route() was used rather than GatewayRouterFunctions
    
    Fixes gh-3265

---
+++ spring-cloud-gateway-server-mvc/src/test/java/org/springframework/cloud/gateway/server/mvc/VanillaRouterFunctionTests.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2013-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.gateway.server.mvc;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.cloud.gateway.server.mvc.test.HttpbinTestcontainers;
+import org.springframework.cloud.gateway.server.mvc.test.HttpbinUriResolver;
+import org.springframework.cloud.gateway.server.mvc.test.TestLoadBalancerConfig;
+import org.springframework.cloud.gateway.server.mvc.test.client.TestRestClient;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.RouterFunctions;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.modifyRequestBody;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+import static org.springframework.cloud.gateway.server.mvc.predicate.GatewayRequestPredicates.host;
+
+@SuppressWarnings("unchecked")
+@SpringBootTest(properties = { "spring.cloud.gateway.mvc.http-client.type=jdk" },
+		webEnvironment = WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(initializers = HttpbinTestcontainers.class)
+public class VanillaRouterFunctionTests {
+
+	@LocalServerPort
+	int port;
+
+	@Autowired
+	TestRestClient restClient;
+
+	@SuppressWarnings("rawtypes")
+	@Test
+	public void routerFunctionsRouteWorks() {
+		restClient.post().uri("/anything/routerfunctionsroute").header("Host", "www.routerfunctionsroute.org")
+				.bodyValue("hello").exchange().expectStatus().isOk().expectBody(Map.class).consumeWith(result -> {
+					System.out.println();
+				});
+	}
+
+	@SpringBootConfiguration
+	@EnableAutoConfiguration
+	@LoadBalancerClient(name = "httpbin", configuration = TestLoadBalancerConfig.Httpbin.class)
+	protected static class TestConfiguration {
+
+		@Bean
+		public RouterFunction<ServerResponse> routerFunctionsRoute() {
+			// @formatter:off
+			return RouterFunctions.route()
+					.POST("/anything/routerfunctionsroute", host("**.routerfunctionsroute.org"), http())
+					.before(modifyRequestBody(String.class, String.class, null, (request, s) -> s.toUpperCase()))
+					.before(new HttpbinUriResolver())
+					.build();
+			// @formatter:on
+		}
+
+	}
+
+}
