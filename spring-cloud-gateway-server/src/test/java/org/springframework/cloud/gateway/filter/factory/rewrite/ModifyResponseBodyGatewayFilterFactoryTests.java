5c4b4efb3457050b9654ba6d9d1e55262ee0b4c0
Vineet Kala <vineetkala11@gmail.com>
2024-03-19 07:37:13 +0530

    fix: removed unused import from ModifyResponseBodyGatewayFilterFactoryTests.java

--- spring-cloud-gateway-server/src/test/java/org/springframework/cloud/gateway/filter/factory/rewrite/ModifyResponseBodyGatewayFilterFactoryTests.java
+++ spring-cloud-gateway-server/src/test/java/org/springframework/cloud/gateway/filter/factory/rewrite/ModifyResponseBodyGatewayFilterFactoryTests.java
@@ -17,7 +17,6 @@
 package org.springframework.cloud.gateway.filter.factory.rewrite;
 
 import java.net.URI;
-import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
