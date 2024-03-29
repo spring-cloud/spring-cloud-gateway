a44e160ec9c5737d367ed0e7efb62e6e943d5d32
sunhao1256 <sunhao1256@gmail.com>
2024-03-29 17:03:26 +0800

    fix: fix checkstyle error

--- spring-cloud-gateway-server-mvc/src/test/java/org/springframework/cloud/gateway/server/mvc/DisableRemoveContentLengthFilterTests.java
+++ spring-cloud-gateway-server-mvc/src/test/java/org/springframework/cloud/gateway/server/mvc/DisableRemoveContentLengthFilterTests.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.cloud.gateway.server.mvc;
 
 
@@ -5,11 +21,11 @@ import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
-import static org.springframework.cloud.gateway.server.mvc.test.TestUtils.getMap;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.LinkedMultiValueMap;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.cloud.gateway.server.mvc.test.TestUtils.getMap;
 
 
 @SpringBootTest(properties = {"spring.cloud.gateway.mvc.http-client.type=jdk",
