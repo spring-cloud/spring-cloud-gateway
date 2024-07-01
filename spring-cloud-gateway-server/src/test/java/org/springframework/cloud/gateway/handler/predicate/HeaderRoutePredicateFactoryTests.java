da77ec780af4576a6a4e823c4c4621d909a749f4
ohprettyhak <haklee.googl@gmail.com>
2024-07-01 23:04:58 +0900

    Updates HeaderRoutePredicateFactory to use getValuesAsList
    Fixes gh-3447

--- spring-cloud-gateway-server/src/test/java/org/springframework/cloud/gateway/handler/predicate/HeaderRoutePredicateFactoryTests.java
+++ spring-cloud-gateway-server/src/test/java/org/springframework/cloud/gateway/handler/predicate/HeaderRoutePredicateFactoryTests.java
@@ -92,6 +92,14 @@ public class HeaderRoutePredicateFactoryTests extends BaseWebClientTests {
 		assertThat(predicate.toString()).contains("Header: myheader regexp=myregexp");
 	}
 
+	@Test
+	public void headerRouteHandlesCommaSeparatedValues() {
+		testClient.get().uri("/get").header("X-Example-Header", "value1, value2 ,exact_match,value3").exchange()
+				.expectStatus().isOk().expectHeader()
+				.valueEquals(HANDLER_MAPPER_HEADER, RoutePredicateHandlerMapping.class.getSimpleName()).expectHeader()
+				.valueEquals(ROUTE_ID_HEADER, "header_test_comma_separated");
+	}
+
 	@EnableAutoConfiguration
 	@SpringBootConfiguration
 	@Import(DefaultTestConfig.class)
@@ -103,8 +111,10 @@ public class HeaderRoutePredicateFactoryTests extends BaseWebClientTests {
 		@Bean
 		RouteLocator queryRouteLocator(RouteLocatorBuilder builder) {
 			return builder.routes()
-				.route("header_exists_dsl", r -> r.header("X-Foo").filters(f -> f.prefixPath("/httpbin")).uri(uri))
-				.build();
+					.route("header_exists_dsl", r -> r.header("X-Foo").filters(f -> f.prefixPath("/httpbin")).uri(uri))
+					.route("header_test_comma_separated", r -> r.header("X-Example-Header", "exact_match")
+							.filters(f -> f.prefixPath("/httpbin")).uri(uri))
+					.build();
 		}
 
 	}
