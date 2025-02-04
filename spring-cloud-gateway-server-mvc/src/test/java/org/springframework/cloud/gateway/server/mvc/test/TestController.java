14e29f0e4a28c97353c9a8fcd088d23bd5a55ffa
spencergibb <spencer@gibb.us>
2025-02-04 16:29:12 -0500

    Re-enables MultipartEnvironmentPostProcessor
    Closes gh-3527
    
    Signed-off-by: ubest <zhangfeng@sipg.com.cn>

--- spring-cloud-gateway-server-mvc/src/test/java/org/springframework/cloud/gateway/server/mvc/test/TestController.java
+++ spring-cloud-gateway-server-mvc/src/test/java/org/springframework/cloud/gateway/server/mvc/test/TestController.java
@@ -16,27 +16,21 @@
 
 package org.springframework.cloud.gateway.server.mvc.test;
 
-import java.io.IOException;
 import java.util.Enumeration;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ServerWebExchange;
 
 @RestController
@@ -58,35 +52,7 @@ public class TestController {
 		return result;
 	}
 
-	@PostMapping(value = "/post", consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
-			produces = MediaType.APPLICATION_JSON_VALUE)
-	public Map<String, Object> postFormData(HttpServletRequest request,
-			@RequestParam MultiValueMap<String, MultipartFile> parts) throws ServletException, IOException {
-		HashMap<String, Object> ret = new HashMap<>();
-		ret.put("headers", getHeaders(request));
-		HashMap<String, Object> files = new HashMap<>();
-		ret.put("files", files);
-
-		parts.values().stream().flatMap(List::stream).forEach(part -> {
-			String contentType = part.getContentType();
-			long contentLength = part.getSize();
-			// TODO: get part data
-			files.put(part.getName(), "data:" + contentType + ";base64," + contentLength);
-		});
-		return ret;
-	}
-
-	@PostMapping(path = "/post", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
-			produces = MediaType.APPLICATION_JSON_VALUE)
-	public Map<String, Object> postUrlEncoded(HttpServletRequest request,
-			@RequestBody(required = false) MultiValueMap form) throws IOException {
-		HashMap<String, Object> ret = new HashMap<>();
-		ret.put("headers", getHeaders(request));
-		ret.put("form", form);
-		return ret;
-	}
-
-	@PostMapping(path = "/post", produces = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(path = "/localpost", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Map<String, Object> post(HttpServletRequest request, @RequestBody(required = false) String body) {
 		HashMap<String, Object> ret = new HashMap<>();
 		ret.put("headers", getHeaders(request));
