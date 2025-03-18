0ee6a5d6da432c7d7378329d8281c160e32e1bea
spring-builds <builds@springframework.org>
2025-03-18 23:49:59 +0000

    Going back to snapshots

--- spring-cloud-gateway-server/src/test/java/org/springframework/cloud/gateway/filter/factory/SpringCloudCircuitBreakerFilterFactoryTests.java
+++ spring-cloud-gateway-server/src/test/java/org/springframework/cloud/gateway/filter/factory/SpringCloudCircuitBreakerFilterFactoryTests.java
@@ -246,15 +246,9 @@ public abstract class SpringCloudCircuitBreakerFilterFactoryTests extends BaseWe
 
 	@Test
 	public void filterPostFallback() {
-		testClient.post()
-			.uri("/post")
-			.body(BodyInserters.fromValue("hello"))
-			.header("Host", "www.circuitbreakerfallbackpost.org")
-			.exchange()
-			.expectStatus()
-			.isOk()
-			.expectBody()
-			.json("{\"body\":\"hello\"}");
+		testClient.post().uri("/post").body(BodyInserters.fromValue("hello"))
+				.header("Host", "www.circuitbreakerfallbackpost.org").exchange().expectStatus()
+				.isOk().expectBody().json("{\"body\":\"hello\"}");
 	}
 
 }
