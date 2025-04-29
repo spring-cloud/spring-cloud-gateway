a4bc1dd151e2a18e4551d38887d4ebb9b5fbe607
Olga Maciaszek-Sharma <olga.maciaszek-sharma@broadcom.com>
2025-04-29 15:09:29 +0200

    Update copyright comment.
    Signed-off-by: Olga Maciaszek-Sharma <olga.maciaszek-sharma@broadcom.com>

--- spring-cloud-gateway-server-mvc/src/test/java/org/springframework/cloud/gateway/server/mvc/GatewayServerMvcAutoConfigurationTests.java
+++ spring-cloud-gateway-server-mvc/src/test/java/org/springframework/cloud/gateway/server/mvc/GatewayServerMvcAutoConfigurationTests.java
@@ -1,5 +1,6 @@
 /*
- * Copyright 2013-2025
+ * Copyright 2013-2025 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
