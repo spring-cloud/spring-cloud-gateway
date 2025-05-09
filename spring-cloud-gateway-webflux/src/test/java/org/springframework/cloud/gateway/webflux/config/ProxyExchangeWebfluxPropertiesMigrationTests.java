9cd6b4af32833024dca93718e45adc3b3a4b561e
spencergibb <spencer@gibb.us>
2025-05-09 16:26:54 -0400

    Fixes ProxyExchangeWebfluxPropertiesMigrationTests
    See gh-3362

--- spring-cloud-gateway-webflux/src/test/java/org/springframework/cloud/gateway/webflux/config/ProxyExchangeWebfluxPropertiesMigrationTests.java
+++ spring-cloud-gateway-webflux/src/test/java/org/springframework/cloud/gateway/webflux/config/ProxyExchangeWebfluxPropertiesMigrationTests.java
@@ -33,7 +33,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ProxyExchangeWebfluxPropertiesMigrationTests {
 
 	@Autowired
-	ProxyProperties properties;
+	ProxyExchangeWebfluxProperties properties;
 
 	@Test
 	public void deprecatedRoutePropertiesWork() {
