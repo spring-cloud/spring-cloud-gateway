e40020e290806cf233bd4c63398044d18a88ac86
spring-builds <builds@springframework.org>
2025-06-03 18:55:18 +0000

    Bumping versions

--- spring-cloud-gateway-server-mvc/src/test/java/org/springframework/cloud/gateway/server/mvc/ServerMvcIntegrationTests.java
+++ spring-cloud-gateway-server-mvc/src/test/java/org/springframework/cloud/gateway/server/mvc/ServerMvcIntegrationTests.java
@@ -1049,9 +1049,12 @@ public class ServerMvcIntegrationTests {
 			return new EventController();
 		}
 
-		// TODO This is needed to work around https://github.com/spring-cloud/spring-cloud-gateway/issues/3816
-		// which results from Spring Security being on the classpath.  Once we can address this issue we should
-		// remove this bean and no longer extend WebMvcConfigurationSupport in this configuration class
+		// TODO This is needed to work around
+		// https://github.com/spring-cloud/spring-cloud-gateway/issues/3816
+		// which results from Spring Security being on the classpath. Once we can address
+		// this issue we should
+		// remove this bean and no longer extend WebMvcConfigurationSupport in this
+		// configuration class
 		@Bean
 		@Lazy
 		@Override
