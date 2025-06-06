01f9200f92091820ea13d3f23efd45ee895184d2
Yavor Chamov <yavor300@gmail.com>
2025-06-06 13:22:18 +0300

    Use LinkedHashMap for CORS configurations in CorsGatewayFilterApplicationListener to preserve insertion order. Fixes GH-3805.
    Signed-off-by: Yavor Chamov <yavor300@gmail.com>

--- spring-cloud-gateway-server/src/main/java/org/springframework/cloud/gateway/filter/cors/CorsGatewayFilterApplicationListener.java
+++ spring-cloud-gateway-server/src/main/java/org/springframework/cloud/gateway/filter/cors/CorsGatewayFilterApplicationListener.java
@@ -18,7 +18,6 @@ package org.springframework.cloud.gateway.filter.cors;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
