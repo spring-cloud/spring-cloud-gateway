05703f21daf8049fe4f12d5377a8872167d52ebb
lanbinshen <lanbinshen@xiaomi.com>
2026-03-25 15:02:50 +0800

    Fix issue#3978
    Signed-off-by: lanbinshen <lanbinshen@xiaomi.com>

--- spring-cloud-gateway-server/src/main/java/org/springframework/cloud/gateway/filter/factory/RetryGatewayFilterFactory.java
+++ spring-cloud-gateway-server/src/main/java/org/springframework/cloud/gateway/filter/factory/RetryGatewayFilterFactory.java
@@ -28,7 +28,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Mono;
-import reactor.netty.Connection;
 import reactor.retry.Backoff;
 import reactor.retry.Jitter;
 import reactor.retry.Repeat;
@@ -241,12 +240,6 @@ public class RetryGatewayFilterFactory extends AbstractGatewayFilterFactory<Retr
 	 * Use {@link ServerWebExchangeUtils#reset(ServerWebExchange)}
 	 */
 	public void reset(ServerWebExchange exchange) {
-		Connection conn = exchange.getAttribute(ServerWebExchangeUtils.CLIENT_RESPONSE_CONN_ATTR);
-		if (conn != null) {
-			trace("disposing response connection before next iteration");
-			conn.dispose();
-			exchange.getAttributes().remove(ServerWebExchangeUtils.CLIENT_RESPONSE_CONN_ATTR);
-		}
 		ServerWebExchangeUtils.reset(exchange);
 	}
 
