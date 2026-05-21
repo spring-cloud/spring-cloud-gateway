183cf1ae6b93b52f779fdd8cbaa4230885fa576a
Moritz Halbritter <moritz.halbritter@broadcom.com>
2026-05-21 10:34:04 +0200

    Stabilize WebSocketIntegrationTests.clientClosing test
    Signed-off-by: Moritz Halbritter <moritz.halbritter@broadcom.com>

--- spring-cloud-gateway-server/src/test/java/org/springframework/cloud/gateway/test/websocket/WebSocketIntegrationTests.java
+++ spring-cloud-gateway-server/src/test/java/org/springframework/cloud/gateway/test/websocket/WebSocketIntegrationTests.java
@@ -257,7 +257,10 @@ public class WebSocketIntegrationTests {
 
 	@Test
 	public void clientClosing() throws Exception {
-		this.client.execute(getUrl("/client-close"), session -> session.close(CloseStatus.create(4999, "client-close")))
+		this.client
+				.execute(getUrl("/client-close"),
+						session -> session.receive()
+								.flatMap(msg -> session.close(CloseStatus.create(4999, "client-close"))).then())
 			.block(Duration.ofMillis(5000));
 		assertThat(serverCloseStatusSink.asMono().block(Duration.ofMillis(5000)))
 			.isEqualTo(CloseStatus.create(4999, "client-close"));
@@ -389,7 +392,8 @@ public class WebSocketIntegrationTests {
 
 		@Override
 		public Mono<Void> handle(WebSocketSession session) {
-			return session.closeStatus().doOnNext(serverCloseStatusSink::tryEmitValue).then();
+			return session.send(Mono.just(session.textMessage("ready")))
+					.then(session.closeStatus().doOnNext(serverCloseStatusSink::tryEmitValue).then());
 		}
 
 	}
